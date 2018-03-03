d6d7e144ad083ac33893d9c4a60967604bbc1196
liudonghua123 <liudonghua123@gmail.com>
2018-03-03 20:09:01 +0800

    update login

--- login.py
+++ login.py
@@ -14,9 +14,9 @@ __status__ = 'Development'
 
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from time import sleep
+import os
 
 from selenium.webdriver.chrome.options import Options
 
@@ -25,51 +25,44 @@ def login_and_get_cookies():
     ## create driver
     chrome_options = Options()
 
-    # uncomment if running in visiable mode
-    chrome_options.add_argument('--headless')
+    # read HEADLESS environment to detect whether to use --headless argument
+    if 'HEADLESS' in os.environ:
+        chrome_options.add_argument('--headless')
 
-    driver = webdriver.Chrome(
-        chrome_options=chrome_options, 
-        executable_path='/usr/local/opt/chromedriver/chromedriver')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
 
     ## open home page
     driver.get('http://ehall.ynu.edu.cn')
 
+    # create WebDriverWait instance
+    wait = WebDriverWait(driver, 10)
+
     ## now in home page
-    button = driver.find_element_by_id('ampHasNoLogin')
+    button = wait.until(EC.element_to_be_clickable((By.ID, 'ampHasNoLogin')))
     button.click()
 
     # wait for loading page
-    sleep(3)
-
-    ## now in ids loglin page
-    # set username and password
-    username = driver.find_element_by_id('username')
+    username = wait.until(EC.element_to_be_clickable((By.ID, 'username')))
     password = driver.find_element_by_id('password')
-    submit = driver.find_element_by_class_name('auth_login_btn')
 
-    username.send_keys('')  # TODO an username must be set
-    password.send_keys('')  # TODO a password must be set
-    submit.submit()
-
-    # wait for loading page
-    sleep(3)
+    # fill username with USERNAME environment value
+    username.send_keys(os.environ['USERNAME'])
+    # fill password with USERNAME environment value
+    password.send_keys(os.environ['PASSWORD'])
+    password.submit()
 
     ## if there is Secondary validation
-    if driver.find_element_by_class_name('kick_table'):
+    try:
+        kick_table = driver.find_element_by_class_name('kick_table')
+        wait.until(EC.visibility_of(kick_table))
         continue_btn = driver.find_element_by_class_name('button_ok')
         continue_btn.click()
-    else:
-        pass
-
-    # wait for loading page
-    sleep(3)
-
-    if '新建的桌面' in driver.page_source:
-        cookies = driver.get_cookies()
-        return cookies
-    else:
-        return None
+    finally:
+        if '个人中心' in driver.page_source:
+            cookies = driver.get_cookies()
+            return cookies
+        else:
+            return None
 
 cookies = login_and_get_cookies()
 
