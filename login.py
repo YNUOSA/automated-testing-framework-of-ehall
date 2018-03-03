97efe353fb08b52fd7a530da6f00afb8fdf8de9e
thiswind <thiswind@gmail.com>
2018-03-03 16:15:29 +0800

    init commit

---
+++ login.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''登录ehall.ynu.edu.cn'''
+
+__author__ = 'Hukuang'
+__copyright__ = 'Copyright 2018, YNUOSA'
+
+__lisense__ = 'GPL'
+__version__ = '0.0.1'
+__maintainer__ = 'Hukuang'
+__email__ = 'thiswind@gmail.com'
+__status__ = 'Development'
